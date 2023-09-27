9ccb697a0bd42488910ee300ca4a123a076729eb
ElyPoli <itsos.poli@gmail.com>
2023-09-27 17:17:46 +0200

    print base train card

--- app/Http/Controllers/TrainController.php
+++ app/Http/Controllers/TrainController.php
@@ -3,10 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Train;
 
 class TrainController extends Controller
 {
     public function home() {
-        return view("home");
+        $currentDate = now();
+
+        $data = Train::where("data_di_partenza", ">=", $currentDate)->get();
+
+		return view('home', [
+            "trainsList" => $data
+        ]);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,27 @@
-@extends("layouts.public")
+@extends('layouts.public')
 
-@section("content")
-    <h1>Laravel - Migration-Seeder</h1>
+@section('content')
+    <div class="container">
+        <h1>Laravel - Migration-Seeder</h1>
+        <div class="row row-cols-2 row-cols-md-3 gy-4">
+            @foreach ($trainsList as $train)
+                <div class="col">
+                    <div class="card">
+                        <div class="card-body">
+                            <ul class="list-unstyled card-text">
+                                <li><span class="fw-bold">Azienda</span> {{ $train->azienda }}</li>
+                                <li><span class="fw-bold">Stazione di partenza: </span> {{ $train->stazione_di_partenza }}</li>
+                                <li><span class="fw-bold">Stazione di arrivo: </span> {{ $train->stazione_di_arrivo }}</li>
+                                <li><span class="fw-bold">Orario di partenza: </span> {{ $train->orario_di_partenza }}</li>
+                                <li><span class="fw-bold">Orario di arrivo: </span> {{ $train->orario_di_arrivo }}</li>
+                                <li><span class="fw-bold">Codice treno: </span> {{ $train->numero_carrozze }}</li>
+                                <li><span class="fw-bold">Treno in orario: </span> {{ $train->in_orario }}</li>
+                                <li><span class="fw-bold">Treno cancellato: </span> {{ $train->cancellato }}</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
 @endsection
